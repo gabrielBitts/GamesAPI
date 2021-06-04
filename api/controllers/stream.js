74f22e4e1911e49f9a414da7b4b93dfdb4a9573b
Gabriel Nattis <gabrielbits@live.com>
2021-06-04 11:03:15 -0300

    Uninstalled uuid and created id increment from length

--- api/controllers/stream.js
+++ api/controllers/stream.js
@@ -1,5 +1,3 @@
-var id = require('uuid/v4')
-
 module.exports = () => {
     const streamDB = require('../data/stream.json')
     const controller = {}
@@ -8,12 +6,10 @@ module.exports = () => {
         stream: streamMock
     } = streamDB
 
-    id = id().replace(/[^0-9]/g, '')
-
     controller.listStream = (req, res) => res.status(200).json(streamDB)
     controller.saveStream = (req, res) => {
         streamMock.data.push({
-            id: parseInt(id), 
+            id: streamDB.stream.data.length + 1, 
             date: req.body.date, 
             video: req.body.video
         })
