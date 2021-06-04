d15dcc4a950b5c0134435e54eb97773f99eeff5e
Cleber Otaviano <cleber@w12.com.br>
2021-06-04 16:00:48 -0300

    Integrando com o banco de dados

---
+++ api/models/game.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    year: Number,
+    studio: String,
+    platform: String,
+    dtZeramento: String,
+    tempo: String
+});
+
+module.exports = mongoose.model('Game', gameSchema, 'game');
